e0486ebd2bc61490af7b32ea98dcded9074579c7
Alexandru Rebenciuc <alrebenciuc@gmail.com>
2022-03-29 15:30:14 +0300

    max of attendees

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_29_104708) do
+ActiveRecord::Schema.define(version: 2022_03_29_122341) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 2022_03_29_104708) do
     t.bigint "activity_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "max_of_attendees"
     t.index ["activity_id"], name: "index_courses_on_activity_id"
     t.index ["user_id"], name: "index_courses_on_user_id"
   end
