be0ac21a9ee73abcccd9f92c3dd4e214714f5c85
Valentine Fouche <fouche.valentine@gmail.com>
2022-03-29 15:46:54 +0200

    pour ajouter les doss de view

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,15 +1,24 @@
 class CoursesController < ApplicationController
 
   def index
+    @courses = Course.all
   end
 
   def show
+    @course = Course.find(params[:id])
   end
 
   def new
+    @course = Course.new
   end
 
   def create
+    @course = Course.new(course_params)
+    if @course.save
+      redirect_to course_path(@course), notice: "Course was created!"
+    else
+      render :new
+    end
   end
 
   def destroy
@@ -19,6 +28,12 @@ class CoursesController < ApplicationController
   end
 
   def update
+  end
+
+  private
+
+  def course_params
+    params.require(:course).permit(:name, :duration, :starting_time, :date, :description, :price, :address, :user_id, :activity_id, :max_of_attendees)
   end
 
 end
