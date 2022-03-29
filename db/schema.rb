e0e84407ab20f33019f6bd4cc01911e943e09b39
Valentine Fouche <fouche.valentine@gmail.com>
2022-03-29 12:51:54 +0200

    résolution migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,49 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_29_084307) do
+ActiveRecord::Schema.define(version: 2022_03_29_104708) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "activities", force: :cascade do |t|
+    t.string "name"
+    t.bigint "category_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["category_id"], name: "index_activities_on_category_id"
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "courses", force: :cascade do |t|
+    t.string "name"
+    t.integer "duration"
+    t.date "starting_time"
+    t.date "date"
+    t.text "description"
+    t.integer "price"
+    t.string "address"
+    t.bigint "user_id", null: false
+    t.bigint "activity_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["activity_id"], name: "index_courses_on_activity_id"
+    t.index ["user_id"], name: "index_courses_on_user_id"
+  end
+
+  create_table "reservations", force: :cascade do |t|
+    t.bigint "course_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["course_id"], name: "index_reservations_on_course_id"
+    t.index ["user_id"], name: "index_reservations_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -27,4 +66,9 @@ ActiveRecord::Schema.define(version: 2022_03_29_084307) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "activities", "categories"
+  add_foreign_key "courses", "activities"
+  add_foreign_key "courses", "users"
+  add_foreign_key "reservations", "courses"
+  add_foreign_key "reservations", "users"
 end
