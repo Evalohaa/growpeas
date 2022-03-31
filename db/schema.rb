ee8d451a807c2203d32a56f8c9d1e959c7814e2d
eva lampin <eva.lampin@hotmail.fr>
2022-03-31 17:58:01 +0200

    continue

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_31_122327) do
-
+ActiveRecord::Schema.define(version: 2022_03_31_152709) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
